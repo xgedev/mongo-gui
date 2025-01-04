5e312100903f5625094d1432047cad521776dd5a
Xge <29895712+xgedev@users.noreply.github.com>
2025-01-04 18:46:44 +0100

    Fix

--- src/routes/api/v1/logout/i/+server.js
+++ src/routes/api/v1/logout/i/+server.js
@@ -1,3 +1,5 @@
+import { redirect } from "@sveltejs/kit";
+
 export function GET(event) {
  event.locals.user = null;
  return redirect(307, `/login?redirect=${event.url.searchParams.get("redirect") || encodeURIComponent("/")}`);
