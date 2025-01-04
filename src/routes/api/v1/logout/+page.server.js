9e28af35b28c747a49d08ae952a3752fc2a82371
Xge <29895712+xgedev@users.noreply.github.com>
2025-01-04 18:47:27 +0100

    Logout final fix

--- src/routes/api/v1/logout/+page.server.js
+++ src/routes/api/v1/logout/+page.server.js
@@ -2,5 +2,5 @@ import { redirect } from "@sveltejs/kit";
 
 export function load(event) {
  event.cookies.delete("jwt", { path: "/" });
- return redirect(307, `/logout/i?redirect=${event.url.searchParams.get("redirect") || encodeURIComponent("/")}`);
+ return redirect(307, `/api/v1/logout/i?redirect=${event.url.searchParams.get("redirect") || encodeURIComponent("/")}`);
 }
